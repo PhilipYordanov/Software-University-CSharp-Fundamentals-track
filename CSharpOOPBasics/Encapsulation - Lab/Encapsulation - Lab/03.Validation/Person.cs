0eae9480b93c4385ee86ddbf25ccf8f0e50d3f48
PhilipYordanov <fjordanov1@gmail.com>
2017-07-02 11:38:32 +0300

    - Encapsulation - Lab

---
+++ CSharpOOPBasics/Encapsulation - Lab/Encapsulation - Lab/03.Validation/Person.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot be less than 3 symbols");
+            }
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot be less than 3 symbols");
+            }
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or negative integer");
+            }
+            this.age = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            return this.salary;
+        }
+        private set
+        {
+            if (value < 460.0)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva");
+            }
+            this.salary = value;
+        }
+    }
+
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} get {this.salary:F2} leva";
+    }
+
+    public void IncreaseSalary(double bonus)
+    {
+        if (this.age > 30)
+        {
+            this.salary += this.salary * bonus / 100;
+        }
+        else
+        {
+            this.salary += this.salary * bonus / 200;
+        }
+    }
+}
---
+++ CSharpOOPBasics/Encapsulation - Lab/Encapsulation - Lab/04.Team/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var lines = int.Parse(Console.ReadLine());
+        var team = new Team("Otbor");
+
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            try
+            {
+                var person = new Person(cmdArgs[0],
+                                   cmdArgs[1],
+                                   int.Parse(cmdArgs[2]),
+                                   double.Parse(cmdArgs[3]));
+
+                team.AddPlayer(person);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        Console.WriteLine($"First team have {team.FirstTeam.Count} players");
+        Console.WriteLine($"Reserve team have {team.ReserveTeam.Count} players");
+    }
+}
