b54c8999111620ea260feef463088188a67edf73
PhilipYordanov <fjordanov1@gmail.com>
2017-07-09 16:33:00 +0300

    - Polymorphism - Lab

---
+++ CSharpOOPBasics/Polymorphism - Lab/Polymorphism - Lab/03.Shapes/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Circle circle = new Circle(5);
+        Console.WriteLine(circle.CalculateArea());
+        Console.WriteLine(circle.CalculatePerimeter());
+    }
+}
---
+++ CSharpOOPBasics/Polymorphism - Lab/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿public class Rectangle : Shape
+{
+    private double height;
+    private double width;
+
+    public double Height
+    {
+        get
+        {
+            return this.height;
+        }
+        set
+        {
+            this.height = value;
+        }
+    }
+
+    public double Width
+    {
+        get
+        {
+            return this.width;
+        }
+        set
+        {
+            this.width = value;
+        }
+    }
+
+    public Rectangle(double height, double width)
+    {
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return this.Height * 2 + this.Width * 2;
+    }
+
+    public override double CalculateArea()
+    {
+        return this.Height * this.Width;
+    }
+
+    public override string Draw()
+    {
+        return base.Draw() + "Rectangle";
+    }
+}
+
+   
