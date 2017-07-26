c7d12dd1e2b3ca270316bc73bf8ece324f3182db
PhilipYordanov <fjordanov1@gmail.com>
2017-07-26 13:28:44 +0300

    -Generics - Lab

---
+++ CSharpOOPAdvance/Generics - Lab/Generics - Lab/02.ArrayCreator/StartUp.cs
@@ -0,0 +1,8 @@
+﻿public class StartUp
+{
+    public static void Main()
+    {
+        string[] arrayOfStrings = ArrayCreator.Create(5,"");
+        int[] arrayOfInts = ArrayCreator.Create(10, 0);
+    }
+}
---
+++ CSharpOOPAdvance/Generics - Lab/Generics - Lab/03.Scale/Scale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Scale<T>
+    where T : IComparable<T>
+{
+    private readonly T _first;
+    private readonly T _second;
+
+    public Scale(T first, T second)
+    {
+        this._first = first;
+        this._second = second;
+    }
+
+    public T GetHavier()
+    {
+        var result = this._first.CompareTo(this._second);
+
+        if (result == 0)
+        {
+            return default(T);
+        }
+        if (result > 0)
+        {
+            return this._first;
+        }
+        return this._second;
+        //return result > 0 ? this._first : this._second;
+    }
+}
